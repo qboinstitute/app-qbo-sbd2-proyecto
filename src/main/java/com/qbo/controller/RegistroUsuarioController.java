52cdb615eda7b779b2d57594da0386aa6e895e1d
LUIS SALVATIERRA <luisangel.salvat@gmail.com>
2021-12-01 18:18:44 -0500

    Uso de sesiones,

--- src/main/java/com/qbo/controller/RegistroUsuarioController.java
+++ src/main/java/com/qbo/controller/RegistroUsuarioController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.qbo.model.bd.Usuario;
 import com.qbo.service.UsuarioService;
@@ -19,6 +21,22 @@ public class RegistroUsuarioController {
 		model.addAttribute("title", "Registrar usuario");
 		model.addAttribute("usuario", new Usuario());
 		model.addAttribute("visualizar", false);
+		return "registrarusuario";
+	}
+	
+	@PostMapping("/registrar")
+	public String registrarUsuario(@ModelAttribute("usuario") Usuario objusuaio,
+			Model model) {
+		try {
+			usuarioService.registrarUsuario(objusuaio);
+			model.addAttribute("visualizar", true);
+			model.addAttribute("registro", true);
+			model.addAttribute("mensaje", "Usuario registrado correctamente");
+		}catch (Exception e) {
+			model.addAttribute("visualizar", true);
+			model.addAttribute("registro", false);
+			model.addAttribute("mensaje", "Error al registrar usuario, vuelva intentarlo");
+		}
 		return "registrarusuario";
 	}
 	
