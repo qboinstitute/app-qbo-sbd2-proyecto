77fe811ab5cb83e63c3ba635522d0569d31c4850
LUIS SALVATIERRA <luisangel.salvat@gmail.com>
2021-12-01 22:02:33 -0500

    Mantenimiento completo de la tabla curso.

--- src/main/java/com/qbo/controller/CursoController.java
+++ src/main/java/com/qbo/controller/CursoController.java
@@ -1,5 +1,7 @@
 package com.qbo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -41,6 +43,13 @@ public class CursoController {
 		}
 		return new ResultadoResponse(respuesta, mensaje);
 	}
+	
+	@GetMapping("/listarCursos")
+	@ResponseBody
+	public List<Curso> listarCursos(){
+		return cursoService.listarCursos();
+	}
+	
 	
 	@DeleteMapping("/eliminarCurso")
 	@ResponseBody
